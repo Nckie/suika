da3fa892c2114ba1dbc3f62d70a3692403000562
Nickie <nickie@malt.com>
2020-05-08 18:14:07 +0200

    add simple prop to login page

--- src/main/ui/Login.vue
+++ src/main/ui/Login.vue
@@ -2,17 +2,19 @@
 <div>
     <h2>Connectez-vous à Suika</h2>
     <label for="login-username">Username</label>
-    <input type="text" id="login-username">
+    <input v-model="userName" type="text" id="login-username">
     <label for="login-password">Password</label>
     <input type="text" id="login-password">
 </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue, Prop } from 'vue-property-decorator';
 
 @Component
 export default class Login extends Vue {
+    @Prop({type: String, default: ''})
+    userName!: '';
 
 }
 </script>
