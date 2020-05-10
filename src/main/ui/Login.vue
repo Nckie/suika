f2efc18f468cd8784fe227cb10782294a275b6fa
Nickie <nickie@malt.com>
2020-05-10 11:39:38 +0200

    add pretter to config

--- src/main/ui/Login.vue
+++ src/main/ui/Login.vue
@@ -1,24 +1,37 @@
 <template>
-<div>
-    <h2>Connectez-vous à Suika</h2>
-    <label for="login-username">Username</label>
-    <input v-model="userName" type="text" id="login-username">
-    <label for="login-password">Password</label>
-    <input type="text" id="login-password">
-</div>
+    <div>
+        <h2>Connectez-vous à Suika</h2>
+        <form @submit="checkCredentials">
+            <label for="login-username">Username</label>
+            <input v-model="userName" type="text" id="login-username" placeholder="Identifiant" />
+
+            <label for="login-password">Password</label>
+            <input type="text" id="login-password" placeholder="Mot de passe super sécurisé" />
+
+            <button type="submit">Valider</button>
+        </form>
+    </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue, Prop } from 'vue-property-decorator';
+import { Component, Vue } from 'vue-property-decorator';
 
 @Component
 export default class Login extends Vue {
-    @Prop({type: String, default: ''})
-    userName!: '';
+    private credentialMock = {
+        userName: 'Nickie',
+        password: 'test',
+    };
 
+    private userName = '';
+
+    private password = '';
+
+    checkCredentials() {
+        console.log(this.userName);
+        console.log(this.password);
+    }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
